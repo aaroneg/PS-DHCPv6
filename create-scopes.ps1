d6a9453a82d11bea8d4dc3f15e4044a9d0d92fed
Aaron <aaron@aaroneg.com>
2021-06-01 17:23:27 -0500

    incorporate vlans into scope description

--- create-scopes.ps1
+++ create-scopes.ps1
@@ -3,12 +3,14 @@ Import-Module dhcpserver
 foreach ($Server in $DHCPServers) {
     foreach ($Scope in $DHCP6Scopes) {
         try {
-            $scope=Get-DhcpServerv6Scope -ComputerName $Server -Prefix ($Scope.Network -split '/')[0] -ErrorAction Stop
-            "Scope $($scope.Prefix) on server $($server) exists, skipping."
+            $ScopeObject=Get-DhcpServerv6Scope -ComputerName $Server -Prefix ($Scope.Network -split '/')[0] -ErrorAction Stop
+            "Scope $($ScopeObject.Prefix) on server $($server) exists, skipping."
+            $ScopeObject|Set-DhcpServerv6Scope -Description "Vlan $($Scope.VLAN)" -ComputerName $server
         }
         catch {
             "Scope not found, adding:"
-            Add-DhcpServerv6Scope -ComputerName $Server -Prefix ($Scope.Network -split '/')[0] -Name $Scope.Name -State Active -PassThru
+            $ScopeObject=Add-DhcpServerv6Scope -ComputerName $Server -Prefix ($Scope.Network -split '/')[0] -Name $Scope.Name -State Active -PassThru
+            $ScopeObject|Set-DhcpServerv6Scope -Description "Vlan $($Scope.VLAN)" -ComputerName $server
         }
     }
 }
